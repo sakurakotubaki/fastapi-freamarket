895ca04f6ff370fb7ca90bf8db8214a56cd5d1c6
Jboy <gitonly543@gmail.com>
2024-06-22 21:01:00 +0900

    add: update & delete

--- cruds/item.py
+++ cruds/item.py
@@ -33,10 +33,12 @@ items = [
 ]
 
 
+# 全てのアイテムを取得
 def find_all():
     return items
 
 
+# IDでアイテムを取得
 def find_by_id(id: int):
     for item in items:
         if item.id == id:
@@ -44,6 +46,7 @@ def find_by_id(id: int):
     return None
 
 
+# 大文字の英語をキーワードで検索
 def find_by_name(name: str):
     filtered_items = []
 
@@ -51,3 +54,34 @@ def find_by_name(name: str):
         if name in item.name:
             filtered_items.append(item)
     return filtered_items
+
+
+# アイテムを追加
+def create(item_create):
+    new_item = Item(
+        len(items) + 1,  # len(items)でアイテムの数を取得し、+1で新しいアイテムのIDを設定
+        item_create.get("name"),
+        item_create.get("price"),
+        item_create.get("description"),
+        ItemStatus.ON_SALE
+    )
+    items.append(new_item)
+    return new_item
+
+
+def update(id: int, item_update):
+    for item in items:
+        if item.id == id:
+            item.name = item_update.get("name")
+            item.price = item_update.get("price")
+            item.description = item_update.get("description")
+            return item
+    return None
+
+
+def delete(id: int):
+    for i in range(len(items)):
+        if items[i].id == id:
+            delete_item = items.pop(i)
+            return delete_item
+    return None
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from cruds import item as item_crud
 
 app = FastAPI()
@@ -19,6 +19,24 @@ async def find_by_id(id: int):
 @app.get("/items/")
 async def find_by_name(name: str):
     return item_crud.find_by_name(name)
+
+
+# HTTP POST配列にデータを追加する。リクエストボディで送信される。
+@app.post("/items")
+async def create(item_create=Body()):
+    return item_crud.create(item_create)
+
+
+# HTTP PUTでデータを更新する。リクエストボディで送信される。
+@app.put("/items/{id}")
+async def update(id: int, item_update=Body()):
+    return item_crud.update(id, item_update)
+
+
+# HTTP DELETEでデータを削除する。
+@app.delete("/items/{id}")
+async def delete(id: int):
+    return item_crud.delete(id)
 
 
 if __name__ == "__main__":
